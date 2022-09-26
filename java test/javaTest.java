502596a27099e3bb7aea7c6e4baf8d6bf74845e2
Mohammad-Khan-0 <mtk22@njit.edu>
2022-09-25 17:50:38 -0700

    homework2

---
+++ java test/javaTest.java
@@ -0,0 +1,13 @@
+public class javaTest {
+    
+        public static void main(String[] args) {
+    
+            System.out.println("This is the test text");
+            testmethod();
+        }
+        public static void testmethod(){
+            System.out.println("We made it to the method now we are exiting the program");
+        }
+    
+    
+}
